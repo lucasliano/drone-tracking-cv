0ac8b0c802c23d8c2910f199e4c3158332e7b83e
pedroyanez0201 <pyanez@frba.utn.edu.ar>
2022-08-29 18:40:17 -0300

    windows docker

---
+++ example_linux/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+source /root/px4_ros_com_ros2/install/setup.bash 
+sleep 20
+micrortps_agent start -t ${RTPS_PROTOCOL} -i ${PX4_IP} > /dev/null &
+ros2 run px4_ros_com offboard_control
+
+# colcon build --symlink-install --packages-skip px4_msgs px4_ros_com
+
+exec $@
