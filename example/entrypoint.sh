5bdb8294083d83db7852f4d2dbd3d4905aa8f3db
pedroyanez0201 <pyanez@frba.utn.edu.ar>
2022-08-29 19:35:05 -0300

    fixed entrypoint ros2

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -8,5 +8,6 @@ micrortps_agent start -t ${RTPS_PROTOCOL} -i ${PX4_IP} > /dev/null &
 ros2 run px4_ros_com offboard_control
 
 # colcon build --symlink-install --packages-skip px4_msgs px4_ros_com
-
+#
 exec $@
+
